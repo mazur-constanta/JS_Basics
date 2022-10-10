b47f39893ca495804591139d5c529a7dd7a8be4d
Constanta <ifcmazur@gmail.com>
2022-10-10 18:06:47 +0300

    New script2.js added starting ES6 arrow functions

--- js/script.js
+++ js/script.js
@@ -215,30 +215,143 @@ first: for (let i = 0; i < 3; i++) {
     }
 }*/
 
-for (let i = 5; i <= 10; i++) {
-    console.log(i);
+// function firstTask() {
+//     for (let i = 5; i < 11; i++) {
+//         console.log(i);
+//     }
+// }
+
+// firstTask();
+
+// function secondTask() {
+//     for (let j = 20; j >= 10; j--) {
+//         if (j === 13) break;
+//         console.log(j);
+//     }
+// }
+
+// secondTask();
+
+// function thirdTask() {
+//     for (let k = 2; k <= 10; k++) {
+//         if (k % 2 === 0) {
+//             console.log(k);
+//         } 
+//     }
+// }
+
+// thirdTask();
+
+// function fourthTask() {
+//     let start = 2;
+
+//     while (start <= 16) {
+//         if (start % 2 === 0) {
+//             start++;
+//             continue;
+//         } else {
+//             console.log(start);
+//         }
+//         start++;
+//     }
+// }
+
+// fourthTask();
+
+/*
+function firstTask() {
+    const arr = [3, 5, 8, 16, 20, 23, 50];
+    const result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        result[i] = arr[i];
+    }
+
+    console.log(result);
+    return result;
 }
 
-for (let j = 20; j >= 10; j--) {
-    if (j === 13) {
-        break;
+firstTask();
+
+//
+function secondTask() {
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+    for (let i = 0; i < data.length; i++) {
+        if (typeof(data[i]) === 'number') {
+            data[i] = data[i] * 2;
+        } else if (typeof(data[i]) === 'string') {
+            data[i] = `${data[i]} - done`;
+        }
+    }
+
+    console.log(data);
+    return data;
+}
+
+secondTask();
+
+//
+function thirdTask() {
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+    const result = [];
+
+    for (let i = 1; i <= data.length; i++) {
+        result[i - 1] = data[data.length - i];
+    }
+
+    console.log(result);
+    return result;
+}
+
+thirdTask();
+
+const lines = 5;
+let result = '';
+
+for (let i = 0; i <= lines; i++) {
+    for (let j = 0; j < lines - i; j++) {
+        result += " ";
+    }
+    for (let j = 0; j < 2 * i + 1; j++) {
+        result += "ˆ";
+    }
+    result += "\n";
+}
+
+console.log(result);
+
+
+let numberOfFilms = +prompt("How many movies you've already watched?", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+for (let i = 0; i < 2; i++) {
+    let lastMovie = prompt("What was the last movie you watched?", ""),
+    rate = prompt("How much do you rate it?", "");
+
+    personalMovieDB.movies[lastMovie] = rate;
+}
+
+console.log(personalMovieDB);
+
+for (let i = 0; i < 2; i++) {
+    let lastMovie = prompt("What was the last movie you watched?", ""),
+        rate = prompt("How much do you rate it?", "");
+
+    if (lastMovie != null && rate != null && lastMovie != '' && rate != '' && lastMovie.length < 50) {
+        personalMovieDB.movies[lastMovie] = rate;
+        console.log("done");
     } else {
-        console.log(j);
+        console.log("error");
+        i--;
     }
 }
 
-for (let k = 2; k <= 10; k++) {
-    if (k % 2 === 0) {
-        console.log(k);
-    } 
-}
-
-
-let start = 2;
-
-do {
-    if (start % 2 === 0) {
-        console.log(start);
-    }
-    start++;
-} while (start < 11);
+console.log(personalMovieDB);*/
