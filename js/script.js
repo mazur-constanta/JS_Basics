8c53972dc1d713be015ef8305357bbbe24a2c54b
Constanta <ifcmazur@gmail.com>
2022-10-10 03:15:16 +0300

    Checked data types, basics

---
+++ js/script.js
@@ -0,0 +1,99 @@
+"use strict";
+
+/*let a = 15;
+console.log(a);
+
+let number = 2;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+console.log(-4/0);
+console.log("string" * 5); // not an number
+
+let und;
+console.log(und);
+
+const obj = {
+    name: "Constanta",
+    age: 25,
+    isMarried: true
+};
+
+console.log(obj.name);
+console.log(obj["name"]); // is not used anymore
+
+let arr = ['plum.jpg', 6,  {}, []];
+console.log(arr[1]); // 6
+
+const secArray = [1, 2, 3];
+console.log(secArray[1]); // 2
+
+const object = {
+    a: 1, 
+    b: 2
+};
+
+object.c = '1234';
+console.log(object.c);
+console.log(object['b']);
+
+const arrObj = {
+    a: "a",
+    '2': "b",
+    3: "c",
+    abc: {
+        one: [{}, {}],
+        two: "blabla"
+    }
+};
+
+// alert("Hello");
+
+// const result = confirm("Are you 18+ years?");
+// console.log(result);
+
+// const answer = +prompt("Do you have 18th?", "18");
+// console.log(answer + 5);
+
+const answers = [];
+
+answers[0] = prompt("How is your name?", "");
+answers[1] = prompt("Surname?", "");
+answers[2] = prompt("Age?", "");
+console.log(typeof(answers));
+
+const category = 'toys';
+
+// console.log('http://toys.com/'+ category);
+console.log(`http://toys.com/${category}/5`);
+
+const userName = "Constanta";
+alert(`Hi dear user, ${userName}`);*/
+
+// console.log('arr' + " - object");
+// console.log(4 + " - object");
+// console.log(4 + +"5");
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+  
+// console.log(incr, decr);
+
+++incr;
+--decr;
+
+console.log(++incr, --decr);
+
+console.log(5%2);
+
+console.log(2 * 4 === '8');
+
+const isChecked = true,
+    isClose = false;
+
+console.log(isChecked && !isClose);
