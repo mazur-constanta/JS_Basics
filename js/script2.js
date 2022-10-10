41edf359ce852c2396dd88fc073b6290c0557dcf
Constanta <ifcmazur@gmail.com>
2022-10-10 18:49:28 +0300

    Added functions logic in new script2.js

---
+++ js/script2.js
@@ -0,0 +1,52 @@
+"use strict";
+
+/* FUNCTIONS
+let num = 20;
+
+// Functions
+function showFirstMessage(text) {
+    console.log(text);
+    let num = 10;
+}
+
+showFirstMessage("Hello");
+console.log(num);
+
+function calc(a, b) {
+    return (a + b);
+}
+
+console.log(calc(4, 3));
+console.log(calc(5, 6));
+
+// Function Declaration
+function ret() {
+    let num = 50;
+    // 
+    return num;
+}
+
+const anotherNum = ret();
+
+console.log(anotherNum);
+
+
+// - Function Declaration - function foo() { code } // can be reached before script creation
+// - Function Expression - let foo = function() { code } // can be reached only after variable declaration
+// - Arrow Function () => // does not have its own context (this) 
+
+// Function Expression
+const logger = function() {
+    console.log("hi");
+};
+
+logger();
+
+// Arrow Function
+const calcul = (a, b) => {
+    // console.log(1);
+    return a + b;
+};
+
+console.log(calcul(4, 5));
+*/
